c4ef6efe09150f95bc8fa9347da1bd75b72f0eb0
Christian Poymann <39243383+NTDuckler@users.noreply.github.com>
2018-11-23 21:17:42 +0100

    Add files via upload

--- DuckyPy.py
+++ DuckyPy.py
@@ -4,180 +4,192 @@ class DuckyPy:
     import time
 
     kbd = Keyboard()
-
+    
+    def getkeycodede(self, arg):
+            if arg == "A":
+                return self.Keycode.SHIFT, self.Keycode.A;
+            if arg == "B":
+                return self.Keycode.SHIFT, self.Keycode.B;
+            if arg == "C":
+                return self.Keycode.SHIFT, self.Keycode.C;
+            if arg == "D":
+                return self.Keycode.SHIFT, self.Keycode.D;
+            if arg == "E":
+                return self.Keycode.SHIFT, self.Keycode.E;
+            if arg == "F":
+                return self.Keycode.SHIFT, self.Keycode.F;        
+            if arg == "G":
+                return self.Keycode.SHIFT, self.Keycode.G;
+            if arg == "H":
+                return self.Keycode.SHIFT, self.Keycode.H;
+            if arg == "I":
+                return self.Keycode.SHIFT, self.Keycode.I;            
+            if arg == "J":
+                return self.Keycode.SHIFT, self.Keycode.J;            
+            if arg == "K":
+                return self.Keycode.SHIFT, self.Keycode.K;            
+            if arg == "L":
+                return self.Keycode.SHIFT, self.Keycode.L;
+            if arg == "M":
+                return self.Keycode.SHIFT, self.Keycode.M;
+            if arg == "N":
+                return self.Keycode.SHIFT, self.Keycode.N;
+            if arg == "O":
+                return self.Keycode.SHIFT, self.Keycode.O;
+            if arg == "P":
+                return self.Keycode.SHIFT, self.Keycode.P;
+            if arg == "Q":
+                return self.Keycode.SHIFT, self.Keycode.Q;
+            if arg == "R":
+                return self.Keycode.SHIFT, self.Keycode.R;
+            if arg == "S":
+                return self.Keycode.SHIFT, self.Keycode.S;
+            if arg == "T":
+                return self.Keycode.SHIFT, self.Keycode.T;
+            if arg == "U":
+                return self.Keycode.SHIFT, self.Keycode.U;
+            if arg == "V":
+                return self.Keycode.SHIFT, self.Keycode.V;
+            if arg == "W":
+                return self.Keycode.SHIFT, self.Keycode.W;
+            if arg == "X":
+                return self.Keycode.SHIFT, self.Keycode.X;
+            if arg == "Y":
+                return self.Keycode.SHIFT, self.Keycode.Z;
+            if arg == "Z":
+                return self.Keycode.SHIFT, self.Keycode.Y;
+            if arg == "a":
+                return self.Keycode.A;
+            if arg == "b":
+                return self.Keycode.B;
+            if arg == "c":
+                return self.Keycode.C;
+            if arg == "d":
+                return self.Keycode.D;
+            if arg == "e":
+                return self.Keycode.E;
+            if arg == "f":
+                return self.Keycode.F;        
+            if arg == "g":
+                return self.Keycode.G;
+            if arg == "h":
+                return self.Keycode.H;
+            if arg == "i":
+                return self.Keycode.I;            
+            if arg == "j":
+                return self.Keycode.J;            
+            if arg == "k":
+                return self.Keycode.K;            
+            if arg == "l":
+                return self.Keycode.L;
+            if arg == "m":
+                return self.Keycode.M;
+            if arg == "n":
+                return self.Keycode.N;
+            if arg == "o":
+                return self.Keycode.O;
+            if arg == "p":
+                return self.Keycode.P;
+            if arg == "q":
+                return self.Keycode.Q;
+            if arg == "r":
+                return self.Keycode.R;
+            if arg == "s":
+                return self.Keycode.S;
+            if arg == "t":
+                return self.Keycode.T;
+            if arg == "u":
+                return self.Keycode.U;
+            if arg == "v":
+                return self.Keycode.V;
+            if arg == "w":
+                return self.Keycode.W;
+            if arg == "x":
+                return self.Keycode.X;
+            if arg == "y":
+                return self.Keycode.Z;
+            if arg == "z":
+                return self.Keycode.Y;
+            if arg == " ":
+                return self.Keycode.SPACE;
+            if arg == ".":
+                return self.Keycode.PERIOD;
+            if arg == ":":
+                return self.Keycode.SHIFT, self.Keycode.PERIOD;
+            if arg == ";":
+                return self.Keycode.SHIFT, self.Keycode.COMMA;
+            if arg == ",":
+                return self.Keycode.COMMA;
+            if arg == "/":
+                return self.Keycode.SHIFT, self.Keycode.SEVEN;
+            if arg == "\\":
+                return self.Keycode.RIGHT_ALT, self.Keycode.MINUS; 
+            if arg == "?":
+                return self.Keycode.SHIFT, self.Keycode.MINUS; 
+            if arg == "-":
+                return self.Keycode.FORWARD_SLASH;
+            if arg == "_":
+                return self.Keycode.SHIFT, self.Keycode.FORWARD_SLASH;
+            if arg == "(":
+                return self.Keycode.SHIFT, self.Keycode.EIGHT;
+            if arg == ";":
+                return self.Keycode.SHIFT, self.Keycode.NINE;
+            if arg == "=":
+                return self.Keycode.SHIFT, self.Keycode.ZERO;  
+            if arg == "[":
+                return self.Keycode.RIGHT_ALT, self.Keycode.EIGHT;
+            if arg == "]":
+                return self.Keycode.RIGHT_ALT, self.Keycode.NINE;
+            if arg == "@":
+                return self.Keycode.SHIFT, self.Keycode.TWO;
+            if arg == "%":
+                return self.Keycode.SHIFT, self.Keycode.FIVE;
+            if arg == "1":
+                return self.Keycode.ONE;
+            if arg == "2":
+                return self.Keycode.TWO;
+            if arg == "3":
+                return self.Keycode.THREE;
+            if arg == "4":
+                return self.Keycode.FOUR;
+            if arg == "5":
+                return self.Keycode.FIVE;
+            if arg == "6":
+                return self.Keycode.SIX;
+            if arg == "7":
+                return self.Keycode.SEVEN;
+            if arg == "8":
+                return self.Keycode.EIGHT;
+            if arg == "9":
+                return self.Keycode.NINE;
+            if arg == "0":
+                return self.Keycode.ZERO;
+    
+    def writeOneOrMultiple(self, arg):
+        if isinstance(arg, int):
+            self.kbd.send(arg)
+        elif len(arg) == 2:
+            self.kbd.send(arg[0], arg[1])
+        elif len(arg) == 3:
+            self.kbd.send(arg[0], arg[1], arg[2])
+    
     def string(self, arg):
-        l = list(arg)
-        for x in range(0, len(l)):
-            if l[x] == "A":
-                self.writeLetterU(self.Keycode.A)
-            if l[x] == "B":
-                self.writeLetterU(self.Keycode.B)
-            if l[x] == "C":
-                self.writeLetterU(self.Keycode.C)
-            if l[x] == "D":
-                self.writeLetterU(self.Keycode.D)
-            if l[x] == "E":
-                self.writeLetterU(self.Keycode.E)
-            if l[x] == "F":
-                self.writeLetterU(self.Keycode.F)        
-            if l[x] == "G":
-                self.writeLetterU(self.Keycode.G)
-            if l[x] == "H":
-                self.writeLetterU(self.Keycode.H)
-            if l[x] == "I":
-                self.writeLetterU(self.Keycode.I)            
-            if l[x] == "J":
-                self.writeLetterU(self.Keycode.J)            
-            if l[x] == "K":
-                self.writeLetterU(self.Keycode.K)            
-            if l[x] == "L":
-                self.writeLetterU(self.Keycode.L)
-            if l[x] == "M":
-                self.writeLetterU(self.Keycode.M)
-            if l[x] == "N":
-                self.writeLetterU(self.Keycode.N)
-            if l[x] == "O":
-                self.writeLetterU(self.Keycode.O)
-            if l[x] == "P":
-                self.writeLetterU(self.Keycode.P)
-            if l[x] == "Q":
-                self.writeLetterU(self.Keycode.Q)
-            if l[x] == "R":
-                self.writeLetterU(self.Keycode.R)
-            if l[x] == "S":
-                self.writeLetterU(self.Keycode.S)
-            if l[x] == "T":
-                self.writeLetterU(self.Keycode.T)
-            if l[x] == "U":
-                self.writeLetterU(self.Keycode.U)
-            if l[x] == "V":
-                self.writeLetterU(self.Keycode.V)
-            if l[x] == "W":
-                self.writeLetterU(self.Keycode.W)
-            if l[x] == "X":
-                self.writeLetterU(self.Keycode.X)
-            if l[x] == "Y":
-                self.writeLetterU(self.Keycode.Z)
-            if l[x] == "Z":
-                self.writeLetterU(self.Keycode.Y)
-            if l[x] == "a":
-                self.writeLetter(self.Keycode.A)
-            if l[x] == "b":
-                self.writeLetter(self.Keycode.B)
-            if l[x] == "c":
-                self.writeLetter(self.Keycode.C)
-            if l[x] == "d":
-                self.writeLetter(self.Keycode.D)
-            if l[x] == "e":
-                self.writeLetter(self.Keycode.E)
-            if l[x] == "f":
-                self.writeLetter(self.Keycode.F)        
-            if l[x] == "g":
-                self.writeLetter(self.Keycode.G)
-            if l[x] == "h":
-                self.writeLetter(self.Keycode.H)
-            if l[x] == "i":
-                self.writeLetter(self.Keycode.I)            
-            if l[x] == "j":
-                self.writeLetter(self.Keycode.J)            
-            if l[x] == "k":
-                self.writeLetter(self.Keycode.K)            
-            if l[x] == "l":
-                self.writeLetter(self.Keycode.L)
-            if l[x] == "m":
-                self.writeLetter(self.Keycode.M)
-            if l[x] == "n":
-                self.writeLetter(self.Keycode.N)
-            if l[x] == "o":
-                self.writeLetter(self.Keycode.O)
-            if l[x] == "p":
-                self.writeLetter(self.Keycode.P)
-            if l[x] == "q":
-                self.writeLetter(self.Keycode.Q)
-            if l[x] == "r":
-                self.writeLetter(self.Keycode.R)
-            if l[x] == "s":
-                self.writeLetter(self.Keycode.S)
-            if l[x] == "t":
-                self.writeLetter(self.Keycode.T)
-            if l[x] == "u":
-                self.writeLetter(self.Keycode.U)
-            if l[x] == "v":
-                self.writeLetter(self.Keycode.V)
-            if l[x] == "w":
-                self.writeLetter(self.Keycode.w)
-            if l[x] == "x":
-                self.writeLetter(self.Keycode.X)
-            if l[x] == "y":
-                self.writeLetter(self.Keycode.Z)
-            if l[x] == "z":
-                self.writeLetter(self.Keycode.Y)
-            if l[x] == " ":
-                self.writeLetter(self.Keycode.SPACE)
-            if l[x] == ".":
-                self.writeLetter(self.Keycode.PERIOD)
-            if l[x] == ":":
-                self.writeLetterU(self.Keycode.PERIOD)
-            if l[x] == ";":
-                self.writeLetterU(self.Keycode.COMMA)
-            if l[x] == ",":
-                self.writeLetter(self.Keycode.COMMA)
-            if l[x] == "/":
-                self.writeLetterU(self.Keycode.SEVEN) 
-            if l[x] == "?":
-                self.writeLetterU(self.Keycode.MINUS) 
-            if l[x] == "-":
-                self.writeLetter(self.Keycode.FORWARD_SLASH)
-            if l[x] == "_":
-                self.writeLetterU(self.Keycode.FORWARD_SLASH)
-            if l[x] == "(":
-                self.writeLetterU(self.Keycode.EIGHT)
-            if l[x] == ")":
-                self.writeLetterU(self.Keycode.NINE)
-            if l[x] == "=":
-                self.writeLetterU(self.Keycode.ZERO)  
-            if l[x] == "[":
-                self.writeLetter(self.Keycode.RIGHT_ALT, self.Keycode.EIGHT)
-            if l[x] == "]":
-                self.writeLetter(self.Keycode.RIGHT_ALT, self.Keycode.NINE)
-            if l[x] == "1":
-                self.writeLetter(self.Keycode.ONE)
-            if l[x] == "2":
-                self.writeLetter(self.Keycode.TWO)
-            if l[x] == "3":
-                self.writeLetter(self.Keycode.THREE)
-            if l[x] == "4":
-                self.writeLetter(self.Keycode.FOUR)
-            if l[x] == "5":
-                self.writeLetter(self.Keycode.FIVE)
-            if l[x] == "6":
-                self.writeLetter(self.Keycode.SIX)
-            if l[x] == "7":
-                self.writeLetter(self.Keycode.SEVEN)
-            if l[x] == "8":
-                self.writeLetter(self.Keycode.EIGHT)
-            if l[x] == "9":
-                self.writeLetter(self.Keycode.NINE)
-            if l[x] == "0":
-                self.writeLetter(self.Keycode.ZERO)
-
-    def writeLetter(self, arg):
-        self.kbd.send(arg)
-
-    def writeLetterU(self, arg):
-        self.kbd.send(self.Keycode.SHIFT, arg)
+        le = list(arg)
+        for x in range(0, len(le)):
+            ler = self.getkeycodede(le[x])
+            self.writeOneOrMultiple(ler)
 
     def desktop(self):
-        self.kbd.send(self.Keycode.GUI, Keycode.D)
+        self.kbd.send(self.Keycode.GUI, self.Keycode.D)
 
     def closeApp(self):
-        self.kbd.send(self.Keycode.ALT, Keycode.F4)
+        self.kbd.send(self.Keycode.ALT, self.Keycode.F4)
 
     def windows(self):
         self.kbd.send(self.Keycode.GUI)
 
     def winR(self):
-        self.kbd.send(self.Keycode.GUI, Keycode.R)
+        self.kbd.send(self.Keycode.GUI, self.Keycode.R)
 
     def escape(self):
         self.kbd.send(self.Keycode.ESCAPE)
@@ -190,6 +202,12 @@ class DuckyPy:
 
     def ctrlI(self, arg):
         self.kbd.send(self.Keycode.CONTROL, arg)
-
+        
+    def cmd(self):
+        self.winR()
+        self.time.sleep(0.3)
+        self.string("cmd.exe")
+        self.enter()
+    
     def menu(self):
         self.kbd.send(self.Keycode.SHIFT, self.Keycode.F10)
